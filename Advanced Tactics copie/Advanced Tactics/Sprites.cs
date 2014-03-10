f0ddc78e1804e24d118018c25eba32afe11e5d22
Megakyior <jupoliglote@gmail.com>
2014-03-10 18:43:26 +0100

    viseur

---
+++ Advanced Tactics copie/Advanced Tactics/Sprites.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Advanced_Tactics
+{
+    public abstract class Sprites
+    {
+		private Game game1;
+		
+        Texture2D texture;
+        Rectangle rectangle;
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public Rectangle Rectangle
+        {
+            get { return rectangle; }
+        }
+
+        public Sprites(Point position)
+        {
+            rectangle.X = position.X;
+            rectangle.Y = position.Y;
+
+            //return rectangle;
+        }
+
+        public virtual void LoadContent(ContentManager Content, String fileName)
+        {
+            texture = Content.Load<Texture2D>(fileName);
+            rectangle.Width = texture.Width;
+            rectangle.Height = texture.Height;
+        }
+
+        public void MoveTo(Point point)
+        {
+            rectangle.X = point.X;
+            rectangle.Y = point.Y;
+        }
+		
+		public Game Game
+        {
+            get { return game1; }
+        }
+
+        public ContentManager Content
+        {
+            get { return game1.Content; }
+        }
+		
+		public Sprites()
+        {
+			
+        }
+    }
+
+}
