dabde9535b40fc7096209eec1d0de69b138fdfe3
Uxheal <jupoliglote@gmail.com>
2014-06-12 16:15:28 +0200

    ajout input

---
+++ Advanced Tactics/Advanced Tactics/Inputs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Advanced_Tactics
+{
+    class Inputs
+    {
+        static private MouseState curMouseState = new MouseState();
+        static private MouseState prevMouseState = new MouseState();
+
+        static private KeyboardState curKeyBState = new KeyboardState();
+        static private KeyboardState prevKeyBState = new KeyboardState();
+
+        static public void Update()
+        {
+            prevMouseState = curMouseState;
+            prevKeyBState = curKeyBState;
+
+            curMouseState = Mouse.GetState();
+            curKeyBState = Keyboard.GetState();
+        }
+
+        static public Vector2 getMousePos()
+        {
+            return new Vector2(curMouseState.X, curMouseState.Y);
+        }
+        static public bool Keyd(Keys k)
+        {
+            return curKeyBState.IsKeyDown(k);
+        }
+        static public bool Keyr(Keys k)
+        {
+            return curKeyBState.IsKeyUp(k) && prevKeyBState.IsKeyDown(k);
+        }
+        static public bool isLMBClick()
+        {
+            return curMouseState.LeftButton == ButtonState.Released && prevMouseState.LeftButton == ButtonState.Pressed;
+        }
+    }
+}
